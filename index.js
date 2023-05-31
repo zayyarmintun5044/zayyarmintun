667dbc1da9f49876637a137c3a8a0c2c5116da12
Zayyar Min Tun <zayyarmintun504@gmail.com>
2023-05-31 13:39:03 +0630

    d

---
+++ index.js
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+       <head>
+     <title>CSS Property</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+       </head>
+ <body>
+     <p id="font">This is a CSS Property.</p>
+	 <p id="font">This is a CSS Property.</p>
+	 <p class="txt">This is a CSS Property.</p>
+	 <p class="txt">This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+	 <p>This is a CSS Property.</p>
+ </body>
+</html>
